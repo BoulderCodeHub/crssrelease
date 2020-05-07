b7c9914619c4ae30268172ee9837745b310d613c
Alan Butler <rabutler@usbr.gov>
2020-05-07 12:03:26 -0600

    zip_crss_package() is now exported

--- R/zip_crss_package.R
+++ R/zip_crss_package.R
@@ -1,35 +1,38 @@
-#' Create a zip package for distribution of CRSS. 
-#' 
+#' Create a zip package for distribution of CRSS.
+#'
+#' `zip_crss_package()` creates a zip package that is used to distribute CRSS.
 #' By default, it includes all files in the top level folder, and the following
-#' folders: control, dmi, model, results, ruleset, sct, temp. The dmi folder is 
+#' folders: control, dmi, model, results, ruleset, sct, temp. The dmi folder is
 #' large, and all files are usually not necessary, so will prompt for user input
 #' to determine which folders to include in the dmi folder
 
 #' @param ifolder Folder to zip
 #' @param zip_name Name to save the file as. Should end in .zip. Will save this
-#'   file in the same directory as ifolder, e.g., if ifolder is 
+#'   file in the same directory as ifolder, e.g., if ifolder is
 #'   "C:/CRSS/CRSS.Jan2010", then the zip will be saved in "C:/CRSS"
-#' @param zip_folders The folders to include in the zip. 
-zip_crss_package <- function(ifolder, zip_name, 
-                             zip_folders = c("control", "dmi", "model", 
+#' @param zip_folders The folders to include in the zip.
+#'
+#' @export
+zip_crss_package <- function(ifolder, zip_name,
+                             zip_folders = c("control", "dmi", "model",
                                              "results", "ruleset", "sct", "temp"
                                              ))
 {
   wd <- getwd()
   on.exit(setwd(wd))
-  
+
   assert_that(tools::file_ext(zip_name) == "zip")
-  
+
   setwd(dirname(ifolder))
   bn <- basename(ifolder)
   # get all files and folders
-  
+
   # get the top level files
   all_files <- file.path(bn, setdiff(
-    list.files(ifolder), 
+    list.files(ifolder),
     list.dirs(ifolder, recursive = FALSE, full.names = FALSE)
   ))
-  
+
   message("Getting files...")
   for (ff in zip_folders) {
     if (ff == "dmi") {
@@ -39,21 +42,21 @@ zip_crss_package <- function(ifolder, zip_name,
       n1 <- length(dmi_folders) + 1
       ops <- paste(seq_along(dmi_folders), dmi_folders, sep = " - ")
       ops <- paste(c("Choose which folders to zip:",
-                     "0 - None", 
-                     ops, 
+                     "0 - None",
+                     ops,
                      paste0(n1, " - All of the above"),
-                     "\nEnter as comma seperated, e.g., 2, 4\n"), 
+                     "\nEnter as comma seperated, e.g., 2, 4\n"),
                    collapse = "\n")
       message(ops)
       ui <- readline(" ")
       ui_vals <- parse_ui(ui)
       all_files <- c(all_files, get_dmi_folders(ui_vals, dmi_folders, bn))
-      
+
     } else {
       all_files <- c(
-        all_files, 
+        all_files,
         file.path(
-          bn, ff, 
+          bn, ff,
           list.files(
             file.path(ifolder, ff), include.dirs = TRUE, recursive = TRUE
           )
@@ -66,7 +69,7 @@ zip_crss_package <- function(ifolder, zip_name,
   zip(zip_name, all_files)
 }
 
-# gets the numbers from comma seperated list. 
+# gets the numbers from comma seperated list.
 parse_ui <- function(ui)
 {
   as.numeric(stringr::str_split(ui, ",")[[1]])
@@ -79,8 +82,8 @@ get_dmi_folders <- function(ui_vals, dmi_folders, base_folder)
   } else if (length(ui_vals) == length(dmi_folders) + 1) {
     # all folders and files
     rv <- list.files(
-      file.path(base_folder, "dmi"), 
-      recursive = TRUE, 
+      file.path(base_folder, "dmi"),
+      recursive = TRUE,
       include.dirs = TRUE
     )
   } else {
@@ -92,17 +95,17 @@ get_dmi_folders <- function(ui_vals, dmi_folders, base_folder)
         # it is a folder, so get all files in the folder
         cur_dir <- file.path(base_folder, "dmi", cur)
         rv <- c(
-          rv, 
+          rv,
           file.path(
             cur_dir,
             list.files(cur_dir, recursive = TRUE, include.dirs = TRUE)
           )
         )
-      } else 
+      } else
         rv <- c(rv, file.path(base_folder, "dmi", cur))
     }
-    
+
   }
-  
+
   rv
 }
